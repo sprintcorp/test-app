79606d7304f50652324971f9d43b1ffda8ef76e9
Frederick Adebayo <sprintcorp7@gmail.com>
2022-04-09 13:23:55 +0100

    [interfaces, repositories] created interfaces and repositories for logic abstraction

---
+++ app/Http/Interfaces/UserInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Interfaces;
+
+
+interface UserInterface
+{
+    public function createUser($data);
+    public function updateUser($data,$id);
+    public function getUsers();
+    public function deleteUser($id);
+}
---
+++ app/Http/Repositories/UserRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Http\Repositories;
+
+
+use App\Http\Interfaces\UserInterface;
+
+class UserRepository implements UserInterface
+{
+
+    public function createUser($data)
+    {
+        // TODO: Implement createUser() method.
+    }
+
+    public function updateUser($data, $id)
+    {
+        // TODO: Implement updateUser() method.
+    }
+
+    public function getUsers()
+    {
+        // TODO: Implement getUsers() method.
+    }
+
+    public function deleteUser($id)
+    {
+        // TODO: Implement deleteUser() method.
+    }
+}
