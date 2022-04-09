bd4e56aa0a30b2f4f08ac7d070864c23a7e014de
Frederick Adebayo <sprintcorp7@gmail.com>
2022-04-09 14:48:26 +0100

    [migration,views,seeder] created role seeder, updated migration and auth views

--- database/migrations/2022_04_09_113742_create_permission_table.php
+++ database/migrations/2022_04_09_113742_create_permission_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('permission', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            ['name' => 'Super Admin'],
+            ['name' => 'Admin'],
+            ['name' => 'Employee'],
+            ['name' => 'Hr Admin'],
+        ];
+        Role::insert($roles);
+    }
+}
