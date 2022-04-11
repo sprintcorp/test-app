d3898e1897b354f2b97344470cbd59fda4bddd35
Frederick Adebayo <sprintcorp7@gmail.com>
2022-04-11 16:06:55 +0100

    unistalled realrashid/sweet-alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,6 @@
 
 <script src="{{asset('assets/dist/js/bootstrap.bundle.min.js')}}"></script>
 
-@include('sweetalert::alert')
 <script src="{{asset('js/sidebars.js')}}"></script>
 <script src="{{asset('js/application.js')}}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
